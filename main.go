bfaececba2c3a3f12d4fcc7b4dd74883db1fc7f4
Christopher Ryan <ryanchris28@gmail.com>
2022-05-25 00:13:38 +0200

    Adding case for CNAME

--- main.go
+++ main.go
@@ -10,21 +10,21 @@ import (
 )
 
 type DNSRecord struct {
-    Question []struct {
-        Name string `json: "name"`
-        Type int `json: "type"`
-    } `json: "Question"`
-    Answer []struct {
-        Name string `json: "name"`
-	    Type int `json: "type"`
-	    TTL  int `json: "TTL"`
-	    Data string `json: "data"`
-    } `json: "Answer"`
+	Question []struct {
+		Name string `json: "name"`
+		Type int    `json: "type"`
+	} `json: "Question"`
+	Answer []struct {
+		Name string `json: "name"`
+		Type int    `json: "type"`
+		TTL  int    `json: "TTL"`
+		Data string `json: "data"`
+	} `json: "Answer"`
 }
 
 func resolveDNSGoogle(recordName string) (record_name string, record_type string, record_ttl int, record_value string) {
 
-    resolveQuery := "https://dns.google/resolve?name=" + recordName
+	resolveQuery := "https://dns.google/resolve?name=" + recordName
 	resp, err := http.Get(resolveQuery)
 	if err != nil {
 		log.Fatalln(err)
@@ -40,21 +40,24 @@ func resolveDNSGoogle(recordName string) (record_name string, record_type string
 	if err := json.Unmarshal(body, &dnsRecord); err != nil {
 		log.Fatalln("Error Parsing JSON: ", err)
 	}
-    
-    record_name = dnsRecord.Answer[0].Name
-    if dnsRecord.Answer[0].Type == 1 {
-        record_type = "A"
-    }
-    record_ttl = dnsRecord.Answer[0].TTL
-    record_value = dnsRecord.Answer[0].Data
-    
-    return record_name, record_type, record_ttl, record_value
+
+	record_name = dnsRecord.Answer[0].Name
+	switch dnsRecord.Answer[0].Type {
+	case 1:
+		record_type = "A"
+	case 5:
+		record_type = "CNAME"
+	}
+	record_ttl = dnsRecord.Answer[0].TTL
+	record_value = dnsRecord.Answer[0].Data
+
+	return record_name, record_type, record_ttl, record_value
 }
 
 func main() {
 	queryName := flag.String("n", "example.com", "The name of the record you wish to resolve")
 	flag.Parse()
-    
-    fmt.Println(resolveDNSGoogle(*queryName))
+
+	fmt.Println(resolveDNSGoogle(*queryName))
 
 }
