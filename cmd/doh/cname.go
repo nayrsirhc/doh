b33f56e811d6111a744edee576c5de768b7aa482
Christopher Ryan <ryanchris28@gmail.com>
2022-10-29 23:20:20 +0200

    feat: add cname command

---
+++ cmd/doh/cname.go
@@ -0,0 +1,21 @@
+package doh
+
+import (
+    "github.com/nayrsirhc/doh/pkg/doh"
+    "github.com/spf13/cobra"
+)
+
+var cnameCmd = &cobra.Command{
+    Use:   "cname",
+    Aliases: []string{"CNAME"},
+    Short:  "Resolves CNAME records for domain",
+    Args:  cobra.ExactArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+        queryName := args[0]
+        doh.RunQuery(queryName,"cname",false)
+    },
+}
+
+func init() {
+    rootCmd.AddCommand(cnameCmd)
+}
