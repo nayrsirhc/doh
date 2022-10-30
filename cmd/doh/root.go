d1ba25b70f889a858c0a4706ce3a8bdd11409af5
Christopher Ryan <ryanchris28@gmail.com>
2022-10-30 21:10:14 +0200

    feat: add version flag

--- cmd/doh/root.go
+++ cmd/doh/root.go
@@ -5,12 +5,15 @@ import (
 	// "github.com/nayrsirhc/doh/pkg/doh"
 )
 
+var version = "0.0.1"
+
 var rootCmd = &cobra.Command{
     Use:  "doh [command] [domain]",
     Short: "doh - a simple CLI to transform and inspect strings",
     Long: `doh is a super fancy CLI (kidding)
 
 One can use doh to modify or inspect strings straight from the terminal`,
+    Version: version,
     Run: func(cmd *cobra.Command, args []string) {
     },
 }
